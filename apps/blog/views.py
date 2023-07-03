f4a97d6cd38efc093099a77a60b2e66ba145bb0e
Robert Alvarez <alvarezrobert150@gmail.com>
2023-07-03 09:47:27 -0400

    video 29

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,19 +1,42 @@
 from django.shortcuts import render
+from apps.blog.models import Post, Category
+
 
 def index(request):
-    return render(request, 'index.html')
+    posts = Post.objects.filter(state = True)
+    return render(request, 'index.html', {"posts": posts})
 
 def generals(request):
-    return render(request, 'generals.html')
+    posts = Post.objects.filter(
+        state = True, 
+        category = Category.objects.get(name = "Generales")
+        ) 
+    return render(request, 'generals.html', {"posts": posts})
 
 def programming(request):
-    return render(request, 'programming.html')
+    posts = Post.objects.filter(
+        state = True, 
+        category = Category.objects.get(name = "Programacion")
+    ) 
+    return render(request, 'programming.html', {"posts": posts})
 
 def tutorials(request):
-    return render(request, 'tutorials.html')
+    posts = Post.objects.filter(
+        state = True, 
+        category = Category.objects.get(name = "Tutoriales")
+    ) 
+    return render(request, 'tutorials.html', {"posts": posts})
 
 def technology(request):
-    return render(request, 'technology.html')
+    posts = Post.objects.filter(
+        state = True, 
+        category = Category.objects.get(name = "Tecnologia")
+    ) 
+    return render(request, 'technology.html', {"posts": posts})
 
 def videogame(request):
-    return render(request, 'videogame.html')
+    posts = Post.objects.filter(
+        state = True, 
+        category = Category.objects.get(name = "Video Juego")
+    ) 
+    return render(request, 'videogame.html', {"posts": posts})
